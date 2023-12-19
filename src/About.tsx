dcbb8fe075903b3f93773d11322d50eef47ed64a
Yusuke Kawamoto <novogrammer@gmail.com>
2023-12-19 16:41:33 +0900

    複数のViewを使ってみる。

--- src/About.tsx
+++ src/About.tsx
@@ -1,17 +1,17 @@
 import { Float, PerspectiveCamera,  View } from "@react-three/drei";
 
 
-interface HeroProps{
+interface AboutProps{
   track:React.MutableRefObject<HTMLElement>;
 }
 
-export default function Hero({track}:HeroProps){
+export default function About({track}:AboutProps){
   if(track.current===null){
     return undefined;
   }
   return <>
     <View track={track}>
-      <color attach="background" args={["hotpink"]}/>
+    <color attach="background" args={["skyblue"]}/>
       <PerspectiveCamera makeDefault position={[0,0,5]} fov={30} />
       <ambientLight intensity={0.6} />
       <directionalLight intensity={1.0} position={[0, 3, 5]}/>
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import { Canvas } from '@react-three/fiber'
 import './App.css'
 import Hero from './Hero'
+import { StatsGl } from '@react-three/drei'
+import { useRef } from 'react';
+import About from './About';
 
 function App() {
-
+  const heroDivElementRef = useRef<HTMLDivElement>(null!);
+  const aboutDivElementRef = useRef<HTMLDivElement>(null!);
   return (
     <>
-      <Hero/>
+      <div style={{
+        position:"fixed",
+        left:0,
+        top:0,
+        width:"100%",
+        height:"100%",
+      }}>
+        <Canvas>
+          <StatsGl/>
+          <Hero track={heroDivElementRef}/>
+          <About track={aboutDivElementRef}/>
+
+        </Canvas>
+      </div>
+      <div style={{
+          position:"relative",
+      }}>
+        <div ref={heroDivElementRef} style={{
+          width:"100%",
+          height:"100svh",
+        }} />
+        <div ref={aboutDivElementRef} style={{
+          width:"100%",
+          height:"100svh",
+        }} />
+      </div>
+
     </>
   )
 }
