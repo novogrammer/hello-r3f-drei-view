98065bfdbccc4717f56d42fe8807766864ca25a1
Yusuke Kawamoto <novogrammer@gmail.com>
2023-12-20 20:48:18 +0900

    MeshRefractionMaterialを試す。重たい。

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { TunnelR3f } from './TunnelR3f';
 import { useEffect, useRef } from 'react';
 import { useCountStore } from './CountStore';
 import Scene03 from './Scene03';
+// import Scene04 from './Scene04';
 
 function App() {
   const incrementCount=useCountStore((state)=>state.increseCount);
@@ -42,6 +43,7 @@ function App() {
         <Scene01/>
         <Scene02/>
         <Scene03/>
+        {/* <Scene04/> */}
       </div>
 
     </>
---
+++ src/Scene04.tsx
@@ -0,0 +1,51 @@
+import { Backdrop, CubeCamera, Environment, Float,  MeshRefractionMaterial, PerspectiveCamera,  View } from "@react-three/drei";
+import { useRef } from "react";
+import { TunnelR3f } from "./TunnelR3f";
+
+
+export default function Scene04(){
+  console.log("Scene04");
+  const trackRef = useRef<HTMLDivElement>(null!);
+  return <>
+    <div ref={trackRef} style={{
+      width:"100%",
+      height:"100svh",
+    }} />
+    <TunnelR3f.In>
+      <View track={trackRef}>
+        <PerspectiveCamera makeDefault position={[0,0,5]} fov={30} />
+        <ambientLight intensity={0.2} />
+        <directionalLight castShadow intensity={2.0} position={[0, 3, 5]}/>
+        <Backdrop
+          receiveShadow
+          floor={0.25}
+          segments={20}
+          scale={2}
+          position={[0,-0.75,0]}
+        >
+          <meshStandardMaterial
+          roughness={1}
+          metalness={0}
+          />
+        </Backdrop>
+        <Float>
+          <CubeCamera>
+            {(texture) => (
+              <mesh castShadow>
+                <torusKnotGeometry args={[0.5,0.15,100,16]}/>
+                <MeshRefractionMaterial
+                  envMap={texture}
+                  color="orange"
+                />
+              </mesh>
+            )}
+          </CubeCamera>
+        </Float>
+        <Environment
+        background={true}
+          preset="city"
+        />
+      </View>
+    </TunnelR3f.In>
+  </>;
+}
