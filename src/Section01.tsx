ceaf2dd41b12e15b534ccc696247d7c5fe31d6ae
Yusuke Kawamoto <novogrammer@gmail.com>
2024-01-09 17:51:54 +0900

    npm run devでtsxを保存した時だけ正しく動く。

--- src/Section01.tsx
+++ src/Section01.tsx
@@ -1,6 +1,7 @@
 import { Backdrop, Environment, Float, PerspectiveCamera,  View } from "@react-three/drei";
 import { useRef, useState } from "react";
 import { TunnelR3f } from "./TunnelR3f";
+import { EffectComposer, Noise, Vignette } from "@react-three/postprocessing";
 
 function Box(){
   const [hovered, setHover] = useState(false);
@@ -15,6 +16,36 @@ function Box(){
 
 }
 
+function Scene(){
+  return <>
+    <PerspectiveCamera makeDefault position={[0,0,5]} fov={30} />
+    <ambientLight intensity={0.3} />
+    <directionalLight castShadow intensity={2.0} position={[0, 3, 5]}/>
+    <Backdrop
+      receiveShadow
+      floor={0.25}
+      segments={20}
+      scale={2}
+      position={[0,-0.75,0]}
+    >
+      <meshStandardMaterial/>
+    </Backdrop>
+    <Float>
+      <Box/>
+    </Float>
+    <Environment
+      background={true}
+      preset="city"
+    />
+    <EffectComposer>
+      <Noise opacity={0.5} />
+      <Vignette eskil={false} offset={0.1} darkness={1.1} />
+    </EffectComposer>
+
+  </>;
+
+}
+
 
 export default function Section01(){
   console.log("Section01");
@@ -26,25 +57,7 @@ export default function Section01(){
     }} />
     <TunnelR3f.In>
       <View track={trackRef}>
-        <PerspectiveCamera makeDefault position={[0,0,5]} fov={30} />
-        <ambientLight intensity={0.3} />
-        <directionalLight castShadow intensity={2.0} position={[0, 3, 5]}/>
-        <Backdrop
-          receiveShadow
-          floor={0.25}
-          segments={20}
-          scale={2}
-          position={[0,-0.75,0]}
-        >
-          <meshStandardMaterial/>
-        </Backdrop>
-        <Float>
-          <Box/>
-        </Float>
-        <Environment
-          background={true}
-          preset="city"
-        />
+        <Scene/>
       </View>
     </TunnelR3f.In>
   </>;
